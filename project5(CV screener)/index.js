5b005732113dc0fedca7782d570bb30412b6cad9
muneebaiqbal <124357977+muneebaiqbal@users.noreply.github.com>
2023-02-03 16:32:42 +0500

    Add files via upload

---
+++ project5(CV screener)/index.js
@@ -0,0 +1,90 @@
+console.log("This is my index.js");
+// Data is an array of objects which contains information about the candidates
+const data = [
+    {
+        name: 'Rehan ',
+        age: 18,
+        city: 'Karachi',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/51.jpg'
+    },
+
+    {
+        name: 'Shahid ',
+        age: 28,
+        city: 'Bhawalpur',
+        language: 'JavaScript',
+        framework: 'Angular',
+        image: 'https://randomuser.me/api/portraits/men/54.jpg'
+    },
+
+    {
+        name: 'Camella Cabello',
+        age: 18,
+        city: 'Kashmir',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/women/55.jpg'
+    },
+
+    {
+        name: 'Aisha Rao',
+        age: 45,
+        city: 'Multan',
+        language: 'Python',
+        framework: 'Flask',
+        image: 'https://randomuser.me/api/portraits/women/57.jpg'
+    },
+
+    {
+        name: 'Ahmed',
+        age: 34,
+        city: 'Jehlum',
+        language: 'Go',
+        framework: 'Go Framework',
+        image: 'https://randomuser.me/api/portraits/men/61.jpg'
+    }
+]
+
+
+// CV Iterator
+function cvIterator(profiles){
+    let nextIndex=0;
+    return {
+        next: function(){
+            return nextIndex<profiles.length ?
+            {value: profiles[nextIndex++], done: false} :
+            {done: true}
+        }
+    };
+}
+const candidates = cvIterator(data);
+
+nextCV();
+// Button listener for next button
+const next = document.getElementById('next');
+next.addEventListener('click', nextCV);
+
+
+function nextCV(){
+    const currentCandidate = candidates.next().value;
+    let image = document.getElementById('image');
+    let profile = document.getElementById('profile');
+    if(currentCandidate != undefined){
+    image.innerHTML = `<img src='${currentCandidate.image}'>`;
+    profile.innerHTML = `<ul class="list-group">
+    <li class="list-group-item">Name: ${currentCandidate.name}</li>
+    <li class="list-group-item">${currentCandidate.age} years old</li>
+    <li class="list-group-item">Lives in ${currentCandidate.city}</li>
+    <li class="list-group-item">Primarily works on ${currentCandidate.language}</li>
+    <li class="list-group-item">Uses ${currentCandidate.framework} framework</li>
+  </ul>`;
+    }
+    else{
+        alert('End of candidate applications');
+        window.location.reload();
+    }
+
+}
+
